8280dec4d475fab881b3d3e53c8d1ab15a8ee711
Leo <lionel.beato@gmail.com>
2019-06-10 23:43:21 -0400

    working on car project

---
+++ testing/src/testing/Vehicle.java
@@ -0,0 +1,59 @@
+package testing;
+
+import java.util.*;
+
+public class Vehicle {
+	
+	private String type;
+	private String make;
+	private String model;
+	private String year; 
+	
+	public Vehicle () {
+		
+	}
+	
+	//setters
+	
+	public void setType (String setter) {
+		this.type =  setter;
+	}
+	
+	public void setMake (String setter) {
+		this.make =  setter;
+	}
+	
+	public void setModel (String setter) {
+		this.model =  setter;
+	}
+	
+	public void setYear (String setter) {
+		this.year =  setter;
+	}
+	
+	//getters
+	
+	public String getType() {
+		return this.type;
+	}
+	
+	public String getMake() {
+		return this.make;
+	}
+	
+	public String getModel() {
+		return this.model;
+	}
+	
+	public String getYear() {
+		return this.year;
+	}
+	
+	
+
+		
+		
+	}
+	
+
+
